9e18a518144a826f6d3b1f98b3cfc998a41baa84
abeTavarez <abeincwireless@gmail.com>
2021-11-13 18:31:37 -0500

    pages pagination

--- pages/src/app/app.component.ts
+++ pages/src/app/app.component.ts
@@ -5,6 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'pages';
+  currentPage = 3;
+
+  images = [
+    {
+      title: 'Mexico',
+      url: 'https://images.unsplash.com/photo-1568402102990-bc541580b59f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1287&q=80'
+    },
+    {
+      title: 'Spain',
+      url: 'https://images.unsplash.com/photo-1558642084-fd07fae5282e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1336&q=80'
+    },
+    {
+      title: 'London',
+      url: 'https://images.unsplash.com/photo-1505761671935-60b3a7427bad?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+      title: 'Japan',
+      url: 'https://images.unsplash.com/photo-1542640244-7e672d6cef4e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80'
+    }
+  ]
 }
